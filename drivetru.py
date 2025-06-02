224251f066db2da7ff72ecd8c5f243c3730e6b9d
adilsiraju <mohd.adil.siraju@gmail.com>
2025-06-02 23:01:05 +0530

    Initial implementation of DriveThru ordering system

---
+++ drivetru.py
@@ -0,0 +1,27 @@
+def get_item(x):
+    menu = {
+        1: "🍔 Cheeseburger",
+        2: "🍟 Fries",
+        3: "🥤 Soda",
+        4: "🍦 Ice Cream",
+        5: "🍪 Cookie"
+    }
+    return menu.get(x, "Sorry, that's not on the menu!")
+
+def welcome():
+    print("Hello!\nWelcome to McD!\nToday's Menu:")
+    print('''
+    1. 🍔 Cheeseburger
+    2. 🍟 Fries
+    3. 🥤 Soda
+    4. 🍦 Ice Cream
+    5. 🍪 Cookie
+    ''')
+
+welcome()
+try:
+    option = int(input("What would you like to order? Enter a number (1-5): "))
+    print(get_item(option))
+    print("You'll recive your order in the next counter.\nThank you for shopping with us.")
+except ValueError:
+    print("Please enter a valid number.")
